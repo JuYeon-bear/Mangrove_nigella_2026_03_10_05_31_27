4591f64fd86c4bc594b90d8e4d05e21873676558
PARK JUYEON <20220904@duksung.ac.kr>
2026-04-29 19:19:44 +0900

    auto-commit

--- sketch.js
+++ sketch.js
@@ -20,13 +20,13 @@ function setup() {
 
 function draw() {
   //맵 그리기
-  image(mapimg, 0, 0);
+  image(mapImg, 0, 0);
 
   //팩맨 움직임
-  if (keyIsDown(LEFT_ARROW)) px -= 3;
-  if (keyIsDown(RIGHT_ARROW)) px += 3;
-  if (keyIsDown(UP_ARROW)) py -= 3;
-  if (keyIsDown(DOWN_ARROW)) py += 3;
+  if (keyIsDown(LEFT_ARROW)&&isWall(px - 3, py)) px -= 3;
+  if (keyIsDown(RIGHT_ARROW)&&isWall(px + 3, py)) px += 3;
+  if (keyIsDown(UP_ARROW)&&isWall(px, py - 3)) py -= 3;
+  if (keyIsDown(DOWN_ARROW)&&isWall(px, py + 3)) py += 3;
 
   if (px <= 5){
     px = 1395;
@@ -35,11 +35,35 @@ function draw() {
   }
 
   fill(255, 255, 0);
+  noStroke();
   ellipse(px, py, pd);
 
 
 
 
+}
+
+function isWall(nextX, nextY){
+  let r = pd / 2;
+
+  let corners = [
+    mapImg.get(nextX - r, nextY),
+    mapImg.get(nextX + r, nextY),
+    mapImg.get(nextX, nextY - r),
+    mapImg.get(nextX, nextY + r),
+  ];
+
+  for (let i = 0; i <corners.length; i++){
+    let c = corners[i];
+
+    if (abs(c[0] - wallR) <= 15 &&
+        abs(c[1] - wallG) <= 15 &&
+        abs(c[2] - wallB) <= 15){
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function mousePressed(){
